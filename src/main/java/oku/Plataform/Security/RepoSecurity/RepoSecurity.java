ecde4bb8d84a139555261264265d8e07bb7dd586
Samba <danielsamba0000@icloud.com>
2026-08-19 09:25:50 +0100

    getting up

--- src/main/java/oku/Plataform/Security/RepoSecurity/RepoSecurity.java
+++ src/main/java/oku/Plataform/Security/RepoSecurity/RepoSecurity.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RepoSecurity extends JpaRepository<Users, String> {
+    Users findByLogin(String login);
 }
--- src/main/java/oku/Plataform/Security/ServiceSecurity/ServiceSecurity.java
+++ src/main/java/oku/Plataform/Security/ServiceSecurity/ServiceSecurity.java
@@ -1,5 +1,10 @@
 package oku.Plataform.Security.ServiceSecurity;
 
+import oku.Plataform.Security.RepoSecurity.RepoSecurity;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.NotFound;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,10 +12,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public class ServiceSecurity implements UserDetailsService {
 
 
-    private
+    private RepoSecurity repo;
+
+    @Autowired
+   public ServiceSecurity(RepoSecurity repo){
+       this.repo=repo;
+   }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return repo.findByLogin(login);
     }
 }
